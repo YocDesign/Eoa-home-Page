b0a888e6784d479cba2684cd40d3fc86336c4bf4
Yoc Design <deepanshu@yoctel.com>
2024-09-24 12:37:01 +0530

    first commit

---
+++ Footer.js
@@ -0,0 +1,92 @@
+//Footer section start
+document.write(`<section class="rv-34-footer rv-37-footer">
+            <div class="container d-none">
+                <div class="row d-none">
+                    <div class="col-lg-4 col-md-6 d-none">
+                        <div class="rv-31-footer-wized rv-31-footer-about mt-0  pr-100 wow fadeInUp">
+                            <a href="index-2.html">
+                                <img src="assets/img/Software/footer/rv-37-footer-logo.png" alt="footer">
+                            </a>
+                            <p class="rv-31-footer-text">
+                                Through a combination of lectures,<br> readings, discussions, students will gain<br>
+                                solid foundation educational.
+                            </p>
+                            <div class="rv-31-footer-socials wow fadeInUp">
+                                <a href="#">
+                                    <span><i class="fa-brands fa-twitter"></i></span>
+                                </a>
+                                <a href="#">
+                                    <span><i class="fa-brands fa-facebook-f"></i></span>
+                                </a>
+                                <a href="#">
+                                    <span><i class="fa-brands fa-linkedin-in"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 col-sm-6 d-none">
+                        <div class="rv-31-footer-wized wow fadeInUp" data-wow-delay="0.25s">
+                            <h4 class="rv-31-footer-wized__title">Useful Links</h4>
+                            <ul class="footer-wized-ul">
+                                <li><a href="about.html">About </a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href="contact.html">Help Center</a></li>
+                                <li><a href="contact.html">Refund</a></li>
+                                <li><a href="about.html">Conditions</a></li>
+                                <li><a href="contact.html">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        </div>
+
+
+                    <div class="col-lg-2 col-md-6 col-sm-6 d-none">
+                        <div class="rv-31-footer-wized wow fadeInUp" data-wow-delay="0.35s">
+                            <h4 class="rv-31-footer-wized__title">Categories</h4>
+                            <ul class="footer-wized-ul">
+                                <li><a href="product-details.html">SaaS Product</a></li>
+                                <li><a href="project-details.html">Development</a></li>
+                                <li><a href="projects.html">Marketing</a></li>
+                                <li><a href="services.html">Servicing</a></li>
+                                <li><a href="product-details.html">Software</a></li>
+                                <li><a href="project-details.html">SaaS Marketing</a></li>
+                            </ul>
+                        </div>
+                        </div>
+
+
+                    <div class="col-lg-4 col-md-6 d-none">
+                        <div class="rv-31-footer-wized wow fadeInUp" data-wow-delay="0.45s">
+                            <h4 class="rv-31-footer-wized__title">Newsletter </h4>
+                            <p class="rv-36-newsletter__text">
+                                Sign up foe our newsletter and get <br> 34% off your next course.
+                            </p>
+                            <form action="#" class="rv-32-footer-nwsltr__form rv-34-footer-newsletter ">
+                                <div class="nwsltr-top">
+                                    <input type="email" required name="email" placeholder="Enter your Email...">
+                                    <button class="rv-34-btn">Subscribe</button>
+                                </div>
+                                <div class="rv-32-footer-nwsltr__checkbox">
+                                    <input type="checkbox" required id="footer-nwsltr-checkbox" name="checkbox"
+                                        value="1">
+                                    <label id="nwsltr-checkbox"> I agree to the <a href="contact.html">Privacy
+                                            Policy</a>.</label>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="rv-34-footer-bottom mt-75 wow fadeInUp">
+                        Copyright © 2024 All Rights Reserved by <a href="https://www.yoctel.com/">Yoctel Solutions Pvt LTD</a>
+                    </div>
+                   
+                                </div>
+                                    </div>
+                                    <div class="row justify-content-between">
+                    <div class="rv-34-footer-bottom wow fadeInUp">
+                        Copyright © 2024 All Rights Reserved by <a href="https://www.yoctel.com/">Yoctel Solutions Pvt LTD</a>
+                    </div>
+                                        </section>`)
+        
+ {/* Footer section end  */}
---
+++ header.js
@@ -0,0 +1,66 @@
+document.write(`<header>
+<div class="rv-37-header">
+    <div class="rv-32-header-top">
+        <img src="assets/img/Software/logo/rv-37-header-img.png" alt="gift">
+        Get 50% off on Exam On Air. Limited time offer!
+    </div>
+    <div class="rv-37-menubar to-be-fixed">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="rv-37-logo">
+                    <a href="#">
+                        <img src="assets/img/Software/logo/logo.jpg" alt="gift">
+                    </a>
+                </div>
+                
+                <div class="rv-1-header-nav__sidebar">
+                    <div
+                        class="sidebar-heading d-lg-none d-flex align-items-center justify-content-between">
+                        <a href="#" class="logo-container "><img
+                                src="assets/img/Software/logo/rv-37-logo.png" alt="logo"></a>
+                        <button class=" rv-1-header-mobile-menu-btn sidebar-close-btn rv-3-def-btn">
+                            <i class="fa-regular fa-xmark"></i>
+                        </button>
+                    </div>
+
+                    <nav class=" rv-37-menu-list rv-1-header__nav">
+                        <ul class="rv-37-menu-list__ul">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class=" title-hot">
+                                <a href="#">How it works
+                                  </a>
+                            </li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">FAQ's</a>
+                            </li>
+                            <li><a href="#">Contact</a>
+                            </li>
+                            <!-- <li><a href="#">Contact</a></li> -->
+                        </ul>
+                    </nav>                  
+
+
+                    
+                     <a href="#myModal" data-bs-toggle="modal" class="rv-33-btn rv-37-btn show-for-devices">Start Your Free Trial
+                         <i class="fa-solid fa-angle-right"></i>
+                     </a>
+
+                </div>
+
+                <div class="rv-31-menu-right">
+                    <a href="#myModal" data-bs-toggle="modal" class="rv-33-btn rv-37-btn hide-for-devices">Start Your Free Trial
+                        <i class="fa-solid fa-angle-right"></i>
+                    </a>
+                    <button class=" rv-3-def-btn rv-1-header-mobile-menu-btn d-lg-none d-inline-block"
+                        id="rv-1-header-mobile-menu-btn">
+                        <i class="fa-bars fa-regular"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</header>`)
+ {/* header section end  */}
